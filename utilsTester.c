5ae38c084740fed60bf4e5a876cec5054b3d3c96
Victor Nguyen <minhnguyennhat0108@gmail.com>
2018-09-20 13:55:26 -0500

    IDK
    sadsadsad

--- utilsTester.c
+++ utilsTester.c
@@ -12,45 +12,116 @@ Date: 09/20/2018
  #define E 1
  int main(int argc, char **argv) {
 
-   double originLatitude = 0;//(argv[1]);
-   double originLongitude = 0;//atof(argv[2]);
-  double destinationLatitude = 1;//atof(argv[3]);
-  double destinationLongitude = 1;//atof(argv[4]);
-//   double t = atof(argv[5]);
-//   double percentC = atof(argv[6]);
-//   if ( originLatitude < -90 || originLatitude > 90 ){
-//     printf("INCORRECT origin latitude points\n" );
-//   }
-//     else if (originLongitude < - 180 || originLongitude > 180) {
-//       printf("INCORRECT origin longitude points\n" );
-//     }
-//     else if (destinationLatitude < - 90 || destinationLatitude > 90) {
-//       printf("INCORRECT destination latitude points\n" );
-//     }
-//     else if (destinationLongitude < - 180 || destinationLongitude > 180) {
-//       printf("INCORRECT destination longitude points\n" );
-//     }
-//
-// double distance = getAirDistance (originLatitude,
-//                  originLongitude,
-//                  destinationLatitude,
-//                 destinationLongitude);
-//
-// printf("Your air distance is: %lf KM \n", distance);
-//
-// double Answer = lorentzTimeDilation (t, percentC);
-// printf("Your time dilation is: %lf \n", Answer
+int casePassed = 0;
+int numCase = 0;
+
+double radians = degreesToRadians(45);
+double radianActual = 0.785398;
+printf("Your answer in radian is: %lf\n", radians);
+
+numCase++;
+if (fabs(radians - radianActual) <= E) {
+  printf("Passed\n");
+  casePassed++;
+}
+else{printf("Failed!!!!!\n");}
+
+radians = degreesToRadians(180);
+radianActual = M_PI;
+printf("Your answer in radian is: %lf\n", radians);
+
+numCase++;
+if (fabs(radians - radianActual) <= E) {
+  printf("Passed\n");
+  casePassed++;
+}
+else{printf("Failed!!!!!\n");}
+
+radians = degreesToRadians(90);
+radianActual = M_PI/2;
+printf("Your answer in radian is: %lf\n", radians);
+
+numCase++;
+if (fabs(radians - radianActual) <= E) {
+  printf("Passed\n");
+  casePassed++;
+}
+else{printf("Failed!!!!!\n");}
+
+
+
 double actual = 157.30;
-double airDist = getAirDistance( originLatitude,
-                       originLongitude,
-                       destinationLatitude,
-                       destinationLongitude);
-airDist = round(airDist*100)/100;
+double airDist = getAirDistance( 0, 0, 1, 1);
 printf("Your air distance is: %lf\n", airDist);
+
+numCase++;
 if (fabs(airDist - actual) <= E) {
   printf("Passed\n");
+  casePassed++;
 
 }
 else {printf("Failed!\n");}
+
+//Test 2
+actual = 1545.24;
+ airDist = getAirDistance( 10, 20, 20, 10);
+printf("Your air distance is: %lf\n", airDist);
+numCase++;
+if (fabs(airDist - actual) <= E) {
+  printf("Passed\n");
+casePassed++;
+}
+else {printf("Failed!\n");}
+
+//Test 3
+ actual = 2621.35;
+airDist = getAirDistance( 40, 60, 60, 40);
+printf("Your air distance is: %lf\n", airDist);
+numCase++;
+if (fabs(airDist - actual) <= E) {
+  printf("Passed\n");
+casePassed++;
+}
+else {printf("Failed!\n");}
+
+double actualTime = 5.773503;
+double Tdiliation = lorentzTimeDilation(5,.5);
+printf("Your time dilation is: %f\n", Tdiliation);
+
+numCase++;
+if (fabs(Tdiliation - actualTime) <= E) {
+  printf("Passed\n");
+  casePassed++;
+
+}
+else {printf("Failed!\n");}
+
+actualTime = 83.3333;
+Tdiliation = lorentzTimeDilation(50,.8);
+printf("Your time dilation is: %f\n", Tdiliation);
+
+numCase++;
+if (fabs(Tdiliation - actualTime) <= E) {
+  printf("Passed\n");
+  casePassed++;
+
+}
+else {printf("Failed!\n");}
+
+actualTime = 60.30227;
+Tdiliation = lorentzTimeDilation(60,.1);
+printf("Your time dilation is: %f\n", Tdiliation);
+
+numCase++;
+if (fabs(Tdiliation - actualTime) <= E) {
+  printf("Passed\n");
+  casePassed++;
+
+}
+else {printf("Failed!\n");}
+
+
+
+
 return 0;
 }
